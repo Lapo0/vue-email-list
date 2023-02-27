de0c4950ed61cee0432d6075418953db55518674
Lapo0 <lapo.gini.01@gmail.com>
2023-02-27 15:22:20 +0100

    FEAT 10 emails

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-        mail: ''
+        email: '',
+        emails: [],
         }
     },
     methods: {
@@ -12,8 +13,14 @@ createApp({
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((res) => {
                 console.log(res.data.response)
-                this.mail = res.data.response
+                this.email = res.data.response
+                this.emails.push(this.email)
         })
         }
     },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            this.createEmail()
+        }
+    }
 }).mount('#app')
