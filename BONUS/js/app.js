7dfe275a8c5954f42f30469cee189fcbe79f4b0a
Lapo0 <lapo.gini.01@gmail.com>
2023-02-27 16:36:00 +0100

    FEAT comment in BONUS

--- BONUS/js/app.js
+++ BONUS/js/app.js
@@ -12,17 +12,23 @@ createApp({
             axios 
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((res) => {
-                console.log(res.data.response)
+                // console.log(res.data.response)
+
+                // assegnare alla mail il nome della mail
                 const email = res.data.response
+
+                // pushare la mail all'interno dell'array
                 this.emails.push(email)
 
+                // condizione per cui se nell'array di mail ci sono 10 mail le può mostrare
                 if (this.emails.length === 10) {
                     this.visibleEmails = true
-                }
+            }
         })
         }
     },
     mounted() {
+        // ciclo per creare le mail da ripetere per 10 volte
         for (let i = 0; i < 10; i++) {
             this.createEmail()
         }
